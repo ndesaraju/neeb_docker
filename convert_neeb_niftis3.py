8c0dc07074122ac9c0ba9b3ea462478923546642
Niharika Desaraju <niharika.desaraju@berkeley.edu>
2019-10-18 19:23:53 -0700

    creating a new repository for the the files required to run Neeb

---
+++ convert_neeb_niftis3.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from subprocess import call
+import glob
+import os
+import sys
+
+subjects = sys.argv[1:]
+# ms????/0?/Neeb/
+
+def main(subject):
+    folders = sorted(glob.glob(subject+'/*'))
+    for folder in folders:
+        print(folder)
+        contents = glob.glob(folder+'/*')
+        if contents and ("ima" in contents[0]): # "ima"
+            files = glob.glob(folder+'/*.ima') # ".ima
+            if (files):
+                rename(files)
+                call(['dcmgz2nii', '-o', folder, folder])
+        else:
+            for content in contents:
+                files = glob.glob(content+'/*.ima') # "ima"
+                if(files):
+                    rename(files)
+                    call(['dcmgz2nii', '-o', content, content])
+    return
+
+def rename(files):
+    for file in files:
+        call(['mv', file, os.path.join(os.path.dirname(file), os.path.basename(file).split('.')[0]+'.DCM')])
+    return
+
+def smooth_maps(subject):
+    myelin = glob.glob(subject+'/Myelin/MyelinMaps/*.nii.gz')[0]
+    for mm in [0.5, 0.8, 1]:
+        call(['fslmaths', myelin, '-s', str(mm), subject+'/Myelin/MyelinMapsSmooth/MyelinMap_Smoothed_'+('%02d' % int(mm*10))+'.nii.gz'])
+
+for subject in subjects:
+    subject = os.path.abspath(subject)
+    print(subject)
+    main(subject)
+    smooth_maps(subject)
+
+
---
+++ rename_files_neeb.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from subprocess import call
+import sys
+import glob
+import os
+
+folder = sys.argv[1]
+list_series = glob.glob(folder+'/*')
+
+for series in list_series:
+
+    list_files = sorted(glob.glob(series+'/*[dD][cC][mM]*'))
+
+    for filename in list_files:
+        print(filename)
+        
+        sans_type = os.path.basename(filename).split('.')[0]
+        
+        if "-" in os.path.basename(list_files[0]):
+            number = int(sans_type.split('-')[-1])
+        else:
+            number = int(sans_type.split('I')[-1][-4:])
+
+        print(series+'/File_anon'+('%04d' % number)+'.ima')
+        call(['mv', filename, series+'/File_anon'+('%04d' % number)+'.ima'])
