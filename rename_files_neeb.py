89c22ad956c3ccef1c4387bf8610f98c14fc64d6
Niharika Desaraju <niharika.desaraju@berkeley.edu>
2019-12-04 00:21:46 -0800

    created counter to rename files regardless of prev name

--- rename_files_neeb.py
+++ rename_files_neeb.py
@@ -10,18 +10,16 @@ list_series = glob.glob(folder+'/*')
 for series in list_series:
 
     list_files = sorted(glob.glob(series+'/*[dD][cC][mM]*'))
-    print("files listed below")
-    print(list_files)
     count = 1
     for filename in list_files:
         print(filename)
         
-        sans_type = os.path.basename(filename).split('.')[0]
+        # sans_type = os.path.basename(filename).split('.')[0]
         
-        if "-" in os.path.basename(list_files[0]):
-            number = int(sans_type.split('-')[-1])
-        else:
-            number = int(sans_type.split('I')[-1][-4:])
+        # if "-" in os.path.basename(list_files[0]):
+        #     number = int(sans_type.split('-')[-1])
+        # else:
+        #     number = int(sans_type.split('I')[-1][-4:])
 
         print(series+'/File_anon'+('%04d' % count)+'.ima')
         call(['mv', filename, series+'/File_anon'+('%04d' % count)+'.ima'])
