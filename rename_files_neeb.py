d1704f7830b3e7136de30e072da3554e0d74e3a1
Niharika Desaraju <niharika.desaraju@berkeley.edu>
2020-02-03 13:34:06 -0800

    debugging renaming error

--- rename_files_neeb.py
+++ rename_files_neeb.py
@@ -24,6 +24,7 @@ for series in list_series:
 
         anon_name = '/File_anon'+('%04d' % count)+'.ima'
         new_path = os.path.join(series, anon_name)
+        print("series: {0}".format(series))
         print(new_path)
         log.info("Renaming {0} to {1}".format(filename, new_path))
         call(['mv', filename, new_path])
